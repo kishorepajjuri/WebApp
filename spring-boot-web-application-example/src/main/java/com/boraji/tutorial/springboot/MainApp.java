d7f863357e1695b0aa311d2af686a5ae031fde26
kishore <kishorjava91@gmail.com>
2020-06-04 13:26:39 +0530

    Added demo message to check
    Signed-off-by: kishore <kishorjava91@gmail.com>

--- spring-boot-web-application-example/src/main/java/com/boraji/tutorial/springboot/MainApp.java
+++ spring-boot-web-application-example/src/main/java/com/boraji/tutorial/springboot/MainApp.java
@@ -2,9 +2,16 @@ package com.boraji.tutorial.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
-public class MainApp {
+public class MainApp extends org.springframework.boot.web.support.SpringBootServletInitializer {
+ 
+    @Override
+    protected SpringApplicationBuilder configure(
+      SpringApplicationBuilder builder) {
+        return builder.sources(MainApp.class);
+    }
    public static void main(String[] args) {
       SpringApplication.run(MainApp.class, args);
    }
